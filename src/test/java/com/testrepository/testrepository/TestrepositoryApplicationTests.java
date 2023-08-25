69ef051957e0030b7ddbbdf4bbebbaa7ae0038bb
Artsiom_Mkrtychan <artsiom_mkrtychan@epam.com>
2023-08-25 12:55:17 +0300

    INit

---
+++ src/test/java/com/testrepository/testrepository/TestrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testrepository.testrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
